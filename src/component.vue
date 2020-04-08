fd343cf696d39a1a7e0dabdfc9fc31091a6f1fc6
Jelle Hak <jellehak@gmail.com>
2020-04-08 10:03:27 +0200

    First commit

---
+++ src/component.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+            counter: 5,
+            initCounter: 5,
+        };
+    },
+}
+</script>
+
+<template>
+    <div class="">
+        <p>The counter is set to <b>{{counter}}</b>.</p>
+        <button @click="counter += 1">Click +1</button>
+        <button @click="counter -= 1">Click -1</button>
+        <button @click="counter = initCounter">Reset</button>
+    </div>
+</template>
